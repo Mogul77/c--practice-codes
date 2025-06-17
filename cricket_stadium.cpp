57859b74c0c272947df81c4f9ee91f6c0b4289d3
Mogul77 <mogulmanoj07@gmail.com>
2025-06-17 15:28:17 +0530

    Add files via upload

---
+++ cricket_stadium.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l;
+    int b;
+    cin>>l;
+    cin>>b;
+    int leng =2*(l+b);
+    int area = l*b;
+    cout<<leng<<endl<<area;
+    
+}
